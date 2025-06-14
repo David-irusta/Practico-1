a5ab2b0c7bc3a3adb1dd5808f069eb88ec8c6a05
David <david.irusta@itscordoba.edu.ar>
2025-06-13 21:14:50 -0300

    Arreglando y diseñando 1

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,25 @@
 const seEntero = document.getElementById("seEntero");
 const bloqueOtroTema = document.getElementById("bloqueOtroTema");
+const formulario = document.getElementById("formulario")
+const boton = document.getElementById("boton")
 
 
-
-seEntero.addEventListener("change", function(){
+seEntero.addEventListener("change", function(event){
     if(seEntero.value === "3"){
         bloqueOtroTema.classList.remove("escondido");
     } else {
         bloqueOtroTema.classList.add("escondido");
     }
 });
-alert("Mensaje enviado")
+
+boton.addEventListener("click", function(event) {
+    event.preventDefault();
+    alert("Mensaje enviado");
+    formulario.reset();
+});
+
+/*seEntero.addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Mensaje enviado");
+    formulario.reset();
+});*/
